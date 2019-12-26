8209b778de38e0fdb919649fb0a48b9d6edf9b74
Daniel <danielkpl2@gmail.com>
2019-12-26 04:45:02 +0000

    Updates

---
+++ precache-manifest.1b31e6024bcc9dae0dd883052b1d8deb.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55e8c273c4e4b9e9fa3eae72f1666ee9",
+    "url": "/react-stars/index.html"
+  },
+  {
+    "revision": "cca73350ef7860ce2c9b",
+    "url": "/react-stars/static/css/main.7ed88b37.chunk.css"
+  },
+  {
+    "revision": "c72bfa7ab36dd01c2ff3",
+    "url": "/react-stars/static/js/2.53d164a9.chunk.js"
+  },
+  {
+    "revision": "c18d434b53ac88970d118259122f8bd8",
+    "url": "/react-stars/static/js/2.53d164a9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cca73350ef7860ce2c9b",
+    "url": "/react-stars/static/js/main.df511d8b.chunk.js"
+  },
+  {
+    "revision": "ee73a42a6712d0851233",
+    "url": "/react-stars/static/js/runtime-main.2c472f5a.js"
+  },
+  {
+    "revision": "bdec0e430af9ed7408ea15f95366b23f",
+    "url": "/react-stars/static/media/milkyway.bdec0e43.jpg"
+  },
+  {
+    "revision": "faa23d4a2133dc6aa1c998ad84b09966",
+    "url": "/react-stars/static/media/spaceage.faa23d4a.otf"
+  },
+  {
+    "revision": "95eeac79f0cee2bb12cfb9b2f201b9e8",
+    "url": "/react-stars/static/media/spinner.95eeac79.gif"
+  }
+]);
